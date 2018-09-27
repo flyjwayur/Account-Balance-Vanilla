17b2e51657992c27a1fc9756fbc776f970edb715
Hye Soo Park <flyjwayur@gmail.com>
2018-09-27 23:15:47 +0300

    Validate user inputs and add new incomes/expenses to account

--- main.js
+++ main.js
@@ -19,14 +19,14 @@ class Account {
     
     addIncome(){
         let description = document.querySelector('.description_input').value;
-        let amount = parseInt(document.querySelector('.amount_input').value);
-        return this.incomes.push({description, amount});
+        let income = parseInt(document.querySelector('.amount_input').value);
+        this.incomes.push({description, income});
     }
 
     addExpense(){
         let description = document.querySelector('.description_input').value;
-        let amount = parseInt(document.querySelector('.amount_input').value);
-        return this.expenses.push({description, amount});
+        let expense = parseInt(document.querySelector('.amount_input').value);
+        this.expenses.push({description, expense});
     }
     
     calTotalIncome(){
@@ -48,8 +48,8 @@ class Account {
     
 }
 
-
-const account1 = new Account([
+// Default values for account;
+const newAccount = new Account([
     { description: "salary", income: 10000  },
     { description: "freelance", income: 5000 },
     { description: "stock", income: 3000 }
@@ -60,19 +60,83 @@ const account1 = new Account([
 ]);
 
 
-const checkInputType = () => {
-    let input_type = document.querySelector('#input_type');
-    console.log(input_type.value);
-    (input_type.value == 'income') ? account1.addIncome() : account1.addExpense();
+//Empty the input with unmatched Regex 
+ const conformInputNumValid = (input) => {
+     let regex = /[^0-9]/;
+     console.log(regex.test(input.value));
+     if(regex.test(input.value)){
+         input.value = input.value.replace(/[^0-9]/,'');
+     }
+ }
+
+const conformInputLetterValid = (input) => {
+     let regex = /[^A-Za-z]/;
+     console.log(regex.test(input.value));
+     if(regex.test(input.value)){
+     input.value = input.value.replace(/[^A-Za-z]/,'');
+ }
+ }
+
+
+//Check validation of inputs and push the data to income & expense array
+const addDataAfterValidation = () => {
+    let input_type = document.querySelector('#input_type').value;
+    let description = document.querySelector('.description_input').value;
+    let income = parseInt(document.querySelector('.amount_input').value);
+    if ((!Number.isNaN(income)) && (description !== "")) {
+        (input_type == 'income') ? newAccount.addIncome() : newAccount.addExpense();
+    }else{
+        alert("Please type description and amount"); 
+    }
 }
 
+//Clean the child element of selected DOM node
+const cleanDisplayedhtml = (income_display, expense_display, balance_display) => {
+    while (income_display.firstChild) {
+        income_display.removeChild(income_display.firstChild);
+    }
+
+    while (expense_display.firstChild) {
+        expense_display.removeChild(expense_display.firstChild);
+    }
+
+    while (balance_display.firstChild) {
+        balance_display.removeChild(balance_display.firstChild);
+    }
+}
+
+//Display the inforamtion of account and remove when display new account
+const displayAccount = () => {
+
+    const income_display = document.querySelector('.income_display');
+    const expense_display = document.querySelector('.expense_display');
+    const balance_display = document.querySelector('.balance_display');
+
+    cleanDisplayedhtml(income_display, expense_display, balance_display);
+
+    for(let {description, income} of newAccount.incomes) {
+        const p = document.createElement('p');
+        p.textContent = `Income: ${description} ${income}`;
+        income_display.appendChild(p);
+    }
+    
+    for(let {description, expense} of newAccount.expenses) {
+        const p = document.createElement('p');
+        p.textContent = `Expense: ${description} ${expense}`;
+        expense_display.appendChild(p);
+    }
+
+    balance_display.textContent = `Net : ${newAccount.totalBalance} \u20AC`;
+    
+}
+
+// Add button and eventlistner 
 const add_info_btn = document.querySelector('.add_info_btn');
+
 add_info_btn.addEventListener("click", () => { 
-    checkInputType();
-    console.log(account1.incomes);
-    console.log(account1.expenses);
+    addDataAfterValidation();
+    displayAccount();
 });
 
-console.log(account1.totalIncome);
-console.log(account1.totalExpense);
-console.log(account1.totalBalance);
+//Default display
+displayAccount();
